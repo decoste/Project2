4c4a603f9d5f78bf92b9f6e4128aedd765d51b52
ala decoste <alshareefialaa@gmail.com>
2021-01-12 16:13:02 -0500

    Removed extra spaces.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -29,8 +29,6 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   
   });
-
-
   // Here we've add our isAuthenticated to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/home", isAuthenticated, function(req, res) {
