8a72cee492daf8f3f696a1b7161ea9406454832a
ala decoste <alshareefialaa@gmail.com>
2021-01-19 21:30:36 -0500

    Added account.js and updated README.

--- models/user.js
+++ models/user.js
@@ -117,62 +117,7 @@ module.exports = function (sequelize, DataTypes) {
       isDecimal: true,
       max: 20,
       min: 8,
-    },
-
-    sv_account_number: {
-      type: DataTypes.INTEGER,
-      unique: true,
-      len: {
-          args: [4,14],
-          msg: "String length is not in this range"
-      }
-    },
-    sv_account_type: {
-        type: DataTypes.INTEGER,
-        unique: true,
-        len: {
-            args: [4,14],
-            msg: "String length is not in this range"
-        }
-    },
-    sv_account_Balance: {
-        type: DataTypes.INTEGER.UNSIGNED,  
-        decimal: [10,2],
-        created_at: sequelize.date, 
-        updated_at: sequelize.date,
-    },
-
-      dda_account_number: {
-        type: DataTypes.INTEGER,
-        unique: true,
-        len: [10],
-    // JSG - we want the Account to Auto Generate
-    },
-    dda_account_type: {
-      type: DataTypes.STRING,
-    },
-
-    dda_account_balance: {
-      type: DataTypes.INTEGER.UNSIGNED,  
-      decimal: [10,2], 
-      created_at: sequelize.date,
-      updated_at: sequelize.date,
-    },
-
-    deposit: {
-      type: DataTypes.INTEGER,
-    },
-    //WD  (-) from account 
-    withdrawal: {
-        type: DataTypes.INTEGER,
-    },
-    //transfer (-)from account (+)to account 
-    transfer: {
-        type: DataTypes.INTEGER,
-    }, 
-      // deducts from acct (-) moves out of bank
-      // wiretransfer: {
-      //}, 
+    } 
   });
 
   User.prototype.validPassword = function (password) {
